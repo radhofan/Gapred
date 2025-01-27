d9a9a3783bc34f59b071dad7ae5c9f1a8e244fa1
Radho <radhofanazizi@gmail.com>
2025-01-27 12:05:24 +0700

    first commit

--- build/lib/gapred/__init__.py
+++ build/lib/gapred/__init__.py
@@ -1,7 +1,7 @@
 # gapred/__init__.py
 
 # Import KNN from the correct location
-from gapred.core.simple_nn import KNN
+from gapred.supervised.knn import KNN
 
 # Explicitly define the public API of the package
 __all__ = ["KNN"]
